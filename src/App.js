fa0bdd502abe42f4c99870caf977d41cf2fba172
Deepansh04 <79331447+Deepansh04@users.noreply.github.com>
2023-03-18 13:06:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Signup from './Components/Signup'
+import Login from './Components/Login'
+import Feed from './Components/Feed'
+import {BrowserRouter,Routes,Route } from 'react-router-dom';
+import {AuthProvider} from './Context/AuthContext.js';
+import PrivateRoute from './Components/PrivateRoute';
+import Profile from './Components/Profile'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' exact element={<PrivateRoute><Feed/></PrivateRoute>} ></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/profile/:id' element={<Profile/>}></Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
